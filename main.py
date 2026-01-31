6d0fc00aae2d2bc8362ebe7262af368398f3632b
VASANTHARAJ-SVR <vasantharaj8133@gmail.com>
2026-01-31 14:33:44 +0530

    Update countdown timer project

--- main.py
+++ main.py
@@ -1,91 +1,42 @@
 import time
 from tkinter import *
 from tkinter import messagebox
-
-
-# creating Tk window
 root = Tk()
-
-# setting geometry of tk window
-root.geometry("300x250")
-
-# Using title() to display a message in
-# the dialogue box of the message in the
-# title bar.
-root.title("Time Counter")
-
-# Declaration of variables
-hour=StringVar()
-minute=StringVar()
-second=StringVar()
-
-# setting the default value as 0
+root.geometry("300x150")
+root.title("Easy Timer")
+hour = StringVar()
+minute = StringVar()
+second = StringVar()
 hour.set("00")
 minute.set("00")
 second.set("00")
-
-# Use of Entry class to take input from the user
-hourEntry= Entry(root, width=3, font=("Arial",18,""),
-				textvariable=hour)
-hourEntry.place(x=80,y=20)
-
-minuteEntry= Entry(root, width=3, font=("Arial",18,""),
-				textvariable=minute)
-minuteEntry.place(x=130,y=20)
-
-secondEntry= Entry(root, width=3, font=("Arial",18,""),
-				textvariable=second)
-secondEntry.place(x=180,y=20)
-
-
-def submit():
-	try:
-		# the input provided by the user is
-		# stored in here :temp
-		temp = int(hour.get())*3600 + int(minute.get())*60 + int(second.get())
-	except:
-		print("Please input the right value")
-	while temp >-1:
-		
-		# divmod(firstvalue = temp//60, secondvalue = temp%60)
-		mins,secs = divmod(temp,60)
-
-		# Converting the input entered in mins or secs to hours,
-		# mins ,secs(input = 110 min --> 120*60 = 6600 => 1hr :
-		# 50min: 0sec)
-		hours=0
-		if mins >60:
-			
-			# divmod(firstvalue = temp//60, secondvalue
-			# = temp%60)
-			hours, mins = divmod(mins, 60)
-		
-		# using format () method to store the value up to
-		# two decimal places
-		hour.set("{0:2d}".format(hours))
-		minute.set("{0:2d}".format(mins))
-		second.set("{0:2d}".format(secs))
-
-		# updating the GUI window after decrementing the
-		# temp value every time
-		root.update()
-		time.sleep(1)
-
-		# when temp value = 0; then a messagebox pop's up
-		# with a message:"Time's up"
-		if (temp == 0):
-			messagebox.showinfo("Time Countdown", "Time's up ")
-		
-		# after every one sec the value of temp will be decremented
-		# by one
-		temp -= 1
-
-# button widget
-btn = Button(root, text='Set Time Countdown', bd='5',
-			command= submit)
-btn.place(x = 70,y = 120)
-
-# infinite loop which is required to
-# run tkinter program infinitely
-# until an interrupt occurs
+Label(root, text="Hours").place(x=30, y=20)
+Label(root, text="Minutes").place(x=120, y=20)
+Label(root, text="Seconds").place(x=210, y=20)
+hour_entry = Entry(root, width=3, textvariable=hour, font=("Arial", 18))
+hour_entry.place(x=30, y=50)
+minute_entry = Entry(root, width=3, textvariable=minute, font=("Arial", 18))
+minute_entry.place(x=120, y=50)
+second_entry = Entry(root, width=3, textvariable=second, font=("Arial", 18))
+second_entry.place(x=210, y=50)
+def start_countdown():
+    try:
+        total_seconds = int(hour.get()) * 3600 + int(minute.get()) * 60 + int(second.get())
+    except:
+        messagebox.showerror("Error", "Enter numbers only!")
+        return
+    while total_seconds >= 0:
+        h = total_seconds // 3600
+        m = (total_seconds % 3600) // 60
+        s = total_seconds % 60
+        hour.set(f"{h:02}")
+        minute.set(f"{m:02}")
+        second.set(f"{s:02}")
+        root.update()
+        time.sleep(1)
+        if total_seconds == 0:
+            messagebox.showinfo("Done!", "Time is up!")
+        total_seconds -= 1
+start_button = Button(root, text="Start", command=start_countdown)
+start_button.place(x=120, y=100)
 root.mainloop()
